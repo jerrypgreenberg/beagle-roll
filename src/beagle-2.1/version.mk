69bd63741a70040b84974b566e54445110ab7fef
Jerry Greenberg <jpg@sdsc.edu>
2014-01-22 12:58:59 -0800

    added v 2.1 but did not remove 1.1
    change author from root to jpg

---
+++ src/beagle-2.1/version.mk
@@ -0,0 +1,8 @@
+NAME    = beagle
+VERSION = 2.1
+RELEASE = 0
+PKGROOT = /opt/beagle/$(VERSION)
+
+SOURCE_NAME        = beagle
+SOURCE_DIR         = $(SOURCE_NAME)_$(VERSION)_src_trestles
+RPM.EXTRAS         = AutoReq:No
--- src/beagle/version.mk
+++ src/beagle/version.mk
@@ -1,5 +1,13 @@
 NAME    = beagle
 VERSION = 1.1
 RELEASE = 0
-SRC_SUBDIR = beagle
-TAR_GZ_PKGS=$(NAME)-$(VERSION).tar.gz
+PKGROOT = /opt/beagle/$(VERSION)
+
+SOURCE_NAME        = beagle
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
+RPM.EXTRAS         = AutoReq:No
