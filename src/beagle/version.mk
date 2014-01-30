bdbb2a5d23d0e8c97919b90f4d03b0ca0aa07f18
Jerry Greenberg <jpg@sdsc.edu>
2014-01-29 17:10:36 -0800

    fixed download path

--- src/beagle/version.mk
+++ src/beagle/version.mk
@@ -8,6 +8,7 @@ SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SRC_SUBDIR         = beagle
 
 TAR_GZ_PKGS        = $(SOURCE_PKG)
 RPM.EXTRAS         = AutoReq:No
